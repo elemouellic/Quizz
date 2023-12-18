927d5fe266685315b7e4bfceaa38476665be9d95
SolennLR <lerouzic.solenn@free.fr>
2023-12-18 16:09:44 +0100

    Maj BDD

--- quizz.sql
+++ quizz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 14 déc. 2023 à 10:38
+-- Généré le : lun. 18 déc. 2023 à 16:06
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,17 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `quizz`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `appartenir`
---
-
-CREATE TABLE `appartenir` (
-  `idSession` int(11) NOT NULL,
-  `idQuestion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -60,6 +49,17 @@ CREATE TABLE `candidats` (
   `idNiveau` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `candidats`
+--
+
+INSERT INTO `candidats` (`idCandidat`, `name`, `lastname`, `dateNaissance`, `login`, `mdp`, `idNiveau`) VALUES
+(1, 'Antoine', 'Griezman', '2000-01-01', 'Griezman.Antoine', '01012000', NULL),
+(2, 'Paul', 'Pogba', '2000-02-01', 'Pogba.Paul', '01022000', NULL),
+(3, 'Florian', 'Thauvin', '2000-03-01', 'Thauvin.Florian', '01032000', NULL),
+(4, 'Hugo', 'Lloris', '2000-04-01', 'Lloris.Hugo', '01042000', NULL),
+(5, 'Kylian', 'Mbappé', '2000-05-01', 'Mbappé.Kylian', '01052000', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -69,19 +69,6 @@ CREATE TABLE `candidats` (
 CREATE TABLE `niveau` (
   `idNiveau` int(11) NOT NULL,
   `libelle` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `participer`
---
-
-CREATE TABLE `participer` (
-  `idCandidat` int(11) NOT NULL,
-  `idSession` int(11) NOT NULL,
-  `dateSession` date NOT NULL,
-  `scorePoint` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -101,6 +88,17 @@ CREATE TABLE `questions` (
   `bonneAnswer` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `questions`
+--
+
+INSERT INTO `questions` (`idQuestion`, `libelle`, `answers1`, `answers2`, `answers3`, `answers4`, `answers5`, `bonneAnswer`) VALUES
+(1, 'Que veut dire faille XSS ?', 'extra-small-small', 'cross-iste-scripting', 'x-box-series-s', NULL, NULL, 2),
+(2, 'Qu\'est ce qu\'un man in the middle ?', 'un milieu de terrain au football', 'un homme ni trop grand ni trop petit', 'une attaque qui a pour but d\'intercepter les communications entre 2 parties', 'la suite malcolm in the middle', NULL, 3),
+(3, 'Qu\'est ce que la plateforme root me ?', 'groot qui aurait perdu son g', 'une plateforme de jardinage', 'une plateforme permettant à chacun de tester ses connaissances en cybersécurité', 'une plateforme d\'outils malveillants gratuits', NULL, 3),
+(4, 'Qu\'est ce que Wannacry ?', 'un groupe de disco des années 70', 'le prochain jeu sur Playstation 6', 'un ransomware', 'une plateforme d\'outils de cyberattaque gratuits', NULL, 3),
+(5, 'Qui a marqué 2 fois en finale de la coupe du monde 98 ?', 'zinedine zidane', 'emmanuel petit', 'christophe dugarry', NULL, NULL, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -108,19 +106,16 @@ CREATE TABLE `questions` (
 --
 
 CREATE TABLE `session` (
-  `idSession` int(6) NOT NULL
+  `idSession` int(6) NOT NULL,
+  `dateSession` date NOT NULL,
+  `ReponseDonnee` int(11) DEFAULT NULL,
+  `idCandidat` int(11) DEFAULT NULL,
+  `idQuestion` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `appartenir`
---
-ALTER TABLE `appartenir`
-  ADD PRIMARY KEY (`idSession`,`idQuestion`),
-  ADD KEY `idQuestion` (`idQuestion`);
 
 --
 -- Index pour la table `bareme`
@@ -142,13 +137,6 @@ ALTER TABLE `niveau`
   ADD PRIMARY KEY (`idNiveau`);
 
 --
--- Index pour la table `participer`
---
-ALTER TABLE `participer`
-  ADD PRIMARY KEY (`idCandidat`,`idSession`),
-  ADD KEY `idSession` (`idSession`);
-
---
 -- Index pour la table `questions`
 --
 ALTER TABLE `questions`
@@ -158,7 +146,9 @@ ALTER TABLE `questions`
 -- Index pour la table `session`
 --
 ALTER TABLE `session`
-  ADD PRIMARY KEY (`idSession`);
+  ADD PRIMARY KEY (`idSession`),
+  ADD KEY `idCandidat` (`idCandidat`) USING BTREE,
+  ADD KEY `idQuestion` (`idQuestion`) USING BTREE;
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -174,7 +164,7 @@ ALTER TABLE `bareme`
 -- AUTO_INCREMENT pour la table `candidats`
 --
 ALTER TABLE `candidats`
-  MODIFY `idCandidat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCandidat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `niveau`
@@ -186,7 +176,7 @@ ALTER TABLE `niveau`
 -- AUTO_INCREMENT pour la table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `idQuestion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idQuestion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `session`
@@ -199,24 +189,17 @@ ALTER TABLE `session`
 --
 
 --
--- Contraintes pour la table `appartenir`
---
-ALTER TABLE `appartenir`
-  ADD CONSTRAINT `appartenir_ibfk_1` FOREIGN KEY (`idSession`) REFERENCES `session` (`idSession`),
-  ADD CONSTRAINT `appartenir_ibfk_2` FOREIGN KEY (`idQuestion`) REFERENCES `questions` (`idQuestion`);
-
---
 -- Contraintes pour la table `candidats`
 --
 ALTER TABLE `candidats`
   ADD CONSTRAINT `candidats_ibfk_1` FOREIGN KEY (`idNiveau`) REFERENCES `niveau` (`idNiveau`);
 
 --
--- Contraintes pour la table `participer`
+-- Contraintes pour la table `session`
 --
-ALTER TABLE `participer`
-  ADD CONSTRAINT `participer_ibfk_1` FOREIGN KEY (`idCandidat`) REFERENCES `candidats` (`idCandidat`),
-  ADD CONSTRAINT `participer_ibfk_2` FOREIGN KEY (`idSession`) REFERENCES `session` (`idSession`);
+ALTER TABLE `session`
+  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`idCandidat`) REFERENCES `candidats` (`idCandidat`),
+  ADD CONSTRAINT `session_ibfk_2` FOREIGN KEY (`idQuestion`) REFERENCES `questions` (`idQuestion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
